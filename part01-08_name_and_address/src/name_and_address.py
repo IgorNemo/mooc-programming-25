c68b0155d090f588eb18ed6f5fd5e932429f68d0
IgorNemo <ig.dan2020@gmail.com>
2025-04-23 16:08:38 +0300

    part01-08 solution

--- part01-08_name_and_address/src/name_and_address.py
+++ part01-08_name_and_address/src/name_and_address.py
@@ -1,1 +1,8 @@
 # Write your solution here
+given_name = input("What is Your Given name? ")
+family_name = input("What is Your Family name? ")
+street_address = input("What is Your Street address? ")
+city_and_postal_code = input("What is Your City and postal code? ")
+print(given_name + " " + family_name)
+print(street_address)
+print(city_and_postal_code)
