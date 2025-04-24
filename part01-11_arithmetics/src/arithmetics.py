04b298b6602ccd0fe589641dc6e3d46d4a8850c8
IgorNemo <ig.dan2020@gmail.com>
2025-04-24 11:32:49 +0300

    part01-11 solution

--- part01-11_arithmetics/src/arithmetics.py
+++ part01-11_arithmetics/src/arithmetics.py
@@ -1,3 +1,11 @@
 # Write your solution here
 x = 27
 y = 15
+summa = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+print(f"{x} + {y} = {summa}")
+print(f"{x} - {y} = {subtraction}")
+print(f"{x} * {y} = {multiplication}")
+print(f"{x} / {y} = {division}")
