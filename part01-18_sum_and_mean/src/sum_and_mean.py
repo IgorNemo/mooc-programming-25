420cf5c5ea1a0a9d5dab8b1e5a2b3682ab86ed6c
IgorNemo <ig.dan2020@gmail.com>
2025-04-25 20:46:09 +0300

    part01-18 solution

--- part01-18_sum_and_mean/src/sum_and_mean.py
+++ part01-18_sum_and_mean/src/sum_and_mean.py
@@ -1,1 +1,8 @@
 # Write your solution here
+number1 = int(input("Please type in the Number 1: "))
+number2 = int(input("Please type in the Number 2: "))
+number3 = int(input("Please type in the Number 3: "))
+number4 = int(input("Please type in the Number 4: "))
+summa = number1 + number2 + number3 + number4
+mean = (number1 + number2 + number3 + number4)/4
+print(f"The sum of the numbers is {summa} and the mean is {mean}")
