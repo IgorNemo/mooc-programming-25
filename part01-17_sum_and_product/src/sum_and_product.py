e127c096e7ece3df19bbd64ad7bd813661ba77ad
IgorNemo <ig.dan2020@gmail.com>
2025-04-25 20:35:47 +0300

    part01-17 solution

--- part01-17_sum_and_product/src/sum_and_product.py
+++ part01-17_sum_and_product/src/sum_and_product.py
@@ -1,1 +1,7 @@
 # Write your solution here
+number1 = int(input("Please type in the first number: "))
+number2 = int(input("Please type in the second number: "))
+summa = number1 + number2
+product = number1 * number2
+print("The sum of the numbers:", summa)
+print("The product of the numbers:", product)
