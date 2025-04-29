f2afc43e5c9192091788e0699653b05941a48fd7
IgorNemo <ig.dan2020@gmail.com>
2025-04-29 12:12:47 +0300

    part01-20 solution

--- part01-20_students_in_groups/src/students_in_groups.py
+++ part01-20_students_in_groups/src/students_in_groups.py
@@ -1,1 +1,9 @@
 # Write your solution here
+quantity_students = int(input("How many students on the course? "))
+desired_group_size = int(input("Desired group size? "))
+a = quantity_students // desired_group_size
+b = quantity_students % desired_group_size
+c = b > 0
+d = int(c)
+number_groups = a + d
+print(f"Number of groups formed: {number_groups}")
