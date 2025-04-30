333892c277c260d39182ea934b26f5e8a3563f1e
IgorNemo <ig.dan2020@gmail.com>
2025-04-30 10:02:26 +0300

    part01-25 solution

--- part01-25_calculator/src/calculator.py
+++ part01-25_calculator/src/calculator.py
@@ -1,1 +1,13 @@
 # Write your solution here
+number1 = int(input("Please input the first number: "))
+number2 = int(input("Please input the second number: "))
+operation = (input("Please input the operation (add, multiply or subtract): "))
+if operation == "add":
+    result = number1 + number2
+    print(f"{number1} + {number2} = {result}")
+if operation == "multiply":
+    result = number1 * number2
+    print(f"{number1} * {number2} = {result}")
+if operation == "subtract":
+    result = number1 - number2
+    print(f"{number1} - {number2} = {result}")
