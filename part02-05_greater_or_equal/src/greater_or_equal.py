1b93839f476c648f8c201674dbb15485788da9d7
IgorNemo <ig.dan2020@gmail.com>
2025-05-05 10:35:55 +0300

    part02-05 solution

--- part02-05_greater_or_equal/src/greater_or_equal.py
+++ part02-05_greater_or_equal/src/greater_or_equal.py
@@ -1,1 +1,10 @@
 # Write your solution here
+number1 = int(input("Please print the first number: "))
+number2 = int(input("Please print the second number: "))
+if (number1 - number2) < 0:
+    print(f"The greater number was: {number2}")
+elif(number1 - number2) > 0:
+    print(f"The greater number was: {number1}")
+else:
+    print(f"The numbers are equal!")
+    
