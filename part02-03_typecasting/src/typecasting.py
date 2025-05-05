bafd381943bd479d6639617b1a0aebc554c18601
IgorNemo <ig.dan2020@gmail.com>
2025-05-05 09:57:02 +0300

    part02-03 solution

--- part02-03_typecasting/src/typecasting.py
+++ part02-03_typecasting/src/typecasting.py
@@ -1,1 +1,4 @@
 # Write your solution here
+number = float(input("Please type in a float number: "))
+print("Rounded down the number is: ", int(number))
+print("Decimail part of the number is: ", float(number - int(number)))
