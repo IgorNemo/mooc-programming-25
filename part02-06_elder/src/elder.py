544f373d04fa6cbb703314f6ef30fcbc8c3cddb3
IgorNemo <ig.dan2020@gmail.com>
2025-05-05 10:53:57 +0300

    part02-06 solution

--- part02-06_elder/src/elder.py
+++ part02-06_elder/src/elder.py
@@ -1,1 +1,11 @@
 # Write your solution here
+name1 = input("Please type in the Name of first person: ")
+age1 = int(input("Please type in the age of the first person: "))
+name2 = input("Please type in the Name of second person: ")
+age2 = int(input("Please type in the age of the second person: "))
+if (age1 > age2):
+    print(f"The elder is {name1}")
+elif(age1 < age2):
+    print(f"The elder is: {name2}")
+else:
+    print(f"{name1} and {name2} are the same age")
