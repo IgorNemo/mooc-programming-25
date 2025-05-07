cfa15409715c1d20a4134ba99e322c03e57951aa
IgorNemo <ig.dan2020@gmail.com>
2025-05-07 16:54:16 +0300

    part02-12 solution

--- part02-12_leap_year/src/leap_year.py
+++ part02-12_leap_year/src/leap_year.py
@@ -1,1 +1,18 @@
 # Write your solution here
+year = int(input("Please type in a year: "))
+if ((year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0))):
+    print("That year is a leap year.")
+else: print("That year is not a leap year.")   
+# if (year % 4 == 0 and year % 100 == 0):
+#     if (year % 400 == 0):
+        #  print("That year is a leap year.")
+# if (year % 4 == 0 and not (year % 100 == 0)):
+#     print("That year is a leap year.")
+# if (not (year % 4 == 0)):
+#     print("That year is not a leap year.")
+# if (year == 500):
+#     print("That year is not a leap year.")
+# if (year == 700):
+#     print("That year is not a leap year.")
+# if (year == 1100):
+#     print("That year is not a leap year.")    
