c367947483896049ee573673bd9a56dc5945f27e
IgorNemo <ig.dan2020@gmail.com>
2025-05-07 17:06:37 +0300

    part02-12 solution

--- part02-12_leap_year/src/leap_year.py
+++ part02-12_leap_year/src/leap_year.py
@@ -5,22 +5,27 @@ if ((year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0))):
 else: print("That year is not a leap year.")  
 
 # Solution from the TMC:
-year = int(input("Please type in a year: "))
+# Start
+
+# year = int(input("Please type in a year: "))
  
-# First, we make assumption that a year is not a leap year
-leap_year = False
+# # First, we make assumption that a year is not a leap year
+# leap_year = False
  
-if year % 100 == 0:
-    if year % 400 == 0:
-        leap_year = True
-elif year % 4 == 0:
-    leap_year = True
+# if year % 100 == 0:
+#     if year % 400 == 0:
+#         leap_year = True
+# elif year % 4 == 0:
+#     leap_year = True
  
-if leap_year:
-    print("That year is a leap year.")
-else:
-    print("That year is not a leap year.")
-  
+# if leap_year:
+#     print("That year is a leap year.")
+# else:
+#     print("That year is not a leap year.")
+
+# End
+
+# Me (not correct program)
 # if (year % 4 == 0 and year % 100 == 0):
 #     if (year % 400 == 0):
         #  print("That year is a leap year.")
