fc26757fc96336a439e0fff32c57987a05723799
IgorNemo <ig.dan2020@gmail.com>
2025-05-07 12:13:29 +0300

    part02-11 solution

--- part02-11_fizzbuzz/src/fizzbuzz.py
+++ part02-11_fizzbuzz/src/fizzbuzz.py
@@ -1,1 +1,8 @@
 # Write your solution here
+number = int(input("Number: "))
+if (number / 3 - number // 3 == 0):
+    if (number / 5 - number // 5 == 0):
+        print("FizzBuzz")
+    else: print("Fizz")
+elif (number / 5 - number // 5 == 0):
+    print("Buzz")
