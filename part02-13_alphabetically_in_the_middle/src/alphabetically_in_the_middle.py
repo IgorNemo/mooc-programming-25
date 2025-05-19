a5282985be9c419e2d497f387e8c6bcfb45bc288
IgorNemo <ig.dan2020@gmail.com>
2025-05-19 10:43:22 +0300

    1

--- part02-13_alphabetically_in_the_middle/src/alphabetically_in_the_middle.py
+++ part02-13_alphabetically_in_the_middle/src/alphabetically_in_the_middle.py
@@ -1,1 +1,13 @@
 # Write your solution here
+first_letter = input("Please type in 1st letter: ")
+second_letter = input("Please type in 2nd letter: ")
+third_letter = input("Please type in 3rd letter: ")
+if ((first_letter < second_letter) and (first_letter > third_letter)):
+    middle_letter = first_letter
+elif ((second_letter < first_letter) and (second_letter > third_letter)):
+        middle_letter = second_letter
+elif ((third_letter < first_letter) and (third_letter > second_letter)):
+        middle_letter = second_letter        
+# else:
+#     middle_letter = third_letter
+print(f"The letter in the middle is {middle_letter}.")
