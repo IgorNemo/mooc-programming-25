991b1f517306cc64958c1f6930076ef18756335d
IgorNemo <ig.dan2020@gmail.com>
2025-07-14 13:17:48 +0300

    part03-07 solution

--- part03-07_consecutive_sum_v2/src/consecutive_sum_v2.py
+++ part03-07_consecutive_sum_v2/src/consecutive_sum_v2.py
@@ -1,1 +1,27 @@
-# Write your solution here
+limit = int(input("Upper limit: "))
+sum = "The consecutive sum: "
+n = 1
+s = 0
+while (s < limit) and (n < limit):
+    s = s + n
+    if n >= (limit - 1) or s >= (limit):
+        sum += f" {n}"
+    else: sum += f" {n} +"
+    n += 1
+sum = sum + f" = {s}"
+print(sum)
+
+
+# Solution from site:
+
+# limit = int(input("Limit: "))
+# number = 1
+# sum = 1
+# numbers = "1"
+# while sum < limit:
+#     number += 1
+#     sum += number
+#     # note that f-string can also be used like this
+#     numbers += f" + {number}"
+# print(f"The consecutive sum: {numbers} = {sum}")
+ 
