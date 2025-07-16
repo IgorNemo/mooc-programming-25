0fff9bae1ee157d64984555932b1b6a0b2f091dd
IgorNemo <ig.dan2020@gmail.com>
2025-07-16 16:38:43 +0300

    part03-13 solution

--- part03-13_rectangle_of_hashes/src/rectangle_of_hashes.py
+++ part03-13_rectangle_of_hashes/src/rectangle_of_hashes.py
@@ -1,1 +1,18 @@
 # Write your solution here
+width = int(input("Width: "))
+height = int(input("Height: "))
+n = 0
+while n < height:
+    print("#"*width)
+    n += 1
+
+# Solution on the site:
+
+# width = int(input("Width: "))
+# height = int(input("Height: "))
+ 
+# n = 0
+# while n < height:
+#     print("#" * width)
+#     n += 1
+ 
