9ae1a753f23f45bfe3920c4d7ae273f9a7c7c446
IgorNemo <ig.dan2020@gmail.com>
2025-07-16 15:58:53 +0300

    part03-10 solution

--- part03-10_end_to_beginning/src/end_to_beginning.py
+++ part03-10_end_to_beginning/src/end_to_beginning.py
@@ -1,1 +1,14 @@
 # Write your solution here
+input_string = input("Please type in a string: ")
+index = len(input_string)
+while index > 0:
+    print(input_string[index - 1])
+    index -= 1
+
+# Solution from the site:
+
+# input_string = input("Please type in a string: ")
+# index = -1
+# while index >= -len(input_string):
+#     print(input_string[index])
+#     index -= 1
