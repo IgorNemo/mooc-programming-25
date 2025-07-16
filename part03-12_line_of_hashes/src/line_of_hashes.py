0b06a76bd5404e16b9ed02a9136b31e87cea0042
IgorNemo <ig.dan2020@gmail.com>
2025-07-16 16:23:59 +0300

    part03-12 solution

--- part03-12_line_of_hashes/src/line_of_hashes.py
+++ part03-12_line_of_hashes/src/line_of_hashes.py
@@ -1,1 +1,15 @@
 # Write your solution here
+number = int(input("Please type in the number of hashes:"))
+n = 0
+hash = ""
+while n < number:
+    hash += "#"
+    n += 1
+print(hash)
+
+# Solution on the site:
+
+# width = int(input("Width: "))
+ 
+# print("#"*width)
+
