044abb9a5485a5d8067dd16c3c00791c970f55f0
IgorNemo <ig.dan2020@gmail.com>
2025-07-16 14:57:55 +0300

    part03-09 slution

--- part03-09_longer_string/src/longer_string.py
+++ part03-09_longer_string/src/longer_string.py
@@ -1,1 +1,14 @@
 # Write your solution here
+string1 = input("Please type in string 1:")
+string2 = input("Please type in string 2:")
+len1 = len(string1)
+len2 = len(string2)
+if (len1 > len2):
+    lagest_string = string1 + " is longer"
+else:
+        if len1 < len2:
+            lagest_string = string2 + " is longer"
+        else:
+            lagest_string = "The strings are equally long"
+print(lagest_string)
+
