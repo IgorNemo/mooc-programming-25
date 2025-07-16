510da288bdf155c6492c8715bd093103a8785069
IgorNemo <ig.dan2020@gmail.com>
2025-07-16 14:25:13 +0300

    part03-08 solution

--- part03-08_string_multiplied/src/string_multiplied.py
+++ part03-08_string_multiplied/src/string_multiplied.py
@@ -1,1 +1,8 @@
 # Write your solution here
+string = input("Please type in a string:")
+amount = int(input("Please type in an amount:"))
+print(string*amount)
+# Solution from Site:
+# input_string = input("Please type in a string: ")
+# amount = int(input("Please type in an amount: "))
+# print (input_string * amount)
