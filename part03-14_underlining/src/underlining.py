6266509f67dd4a0acd3f644969e81b045e6d167e
IgorNemo <ig.dan2020@gmail.com>
2025-07-17 09:45:12 +0300

    part-03-14 solution

--- part03-14_underlining/src/underlining.py
+++ part03-14_underlining/src/underlining.py
@@ -1,1 +1,18 @@
 # Write your solution here
+while True:
+    string = input("Please type in a string: ")
+    if string != "":
+        print(string)
+        print("-" * len(string))
+    else:
+        break
+
+    # Solution from the site:
+
+    # while True:
+    # string = input("Please type in a string: ")
+    # if string == "":
+    #     break
+    # print(string)
+    # print(len(string) * "-")
+
