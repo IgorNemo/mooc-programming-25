8bd3da68b901a37c5eb3ed601fd3dc3fa64f6b2d
IgorNemo <ig.dan2020@gmail.com>
2025-07-17 10:41:38 +0300

    part03-15 solution

--- part03-15_right_aligned/src/right_aligned.py
+++ part03-15_right_aligned/src/right_aligned.py
@@ -1,1 +1,13 @@
 # Write your solution here
+string = input("Please type in a string: ")
+if len(string) == 20:
+        print(string)
+else:
+    print("*" * (20 - len(string)) + string)
+
+
+# Solution from the site:
+
+# word = input("Please type in a string: ") 
+# aligned = (20 - len(word)) * "*" + word 
+# print(aligned)
