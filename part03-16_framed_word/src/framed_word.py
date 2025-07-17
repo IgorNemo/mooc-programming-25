e9a7ca07e421ac5ee4c07204e2674ba5a207a36d
IgorNemo <ig.dan2020@gmail.com>
2025-07-17 17:27:24 +0300

    part03-16 solution

--- part03-16_framed_word/src/framed_word.py
+++ part03-16_framed_word/src/framed_word.py
@@ -1,1 +1,14 @@
 # Write your solution here
+word = input("Word: ")
+p = len(word)
+m = (30 - p)
+n = m // 2
+print("*" * 30)
+if m % 2 == 0:
+    print("*" + " " * (n - 1) + word + " " * (n - 1) + "*")
+else:
+    print("*" + " " * n + word + " " * (n - 1) + "*")
+print("*" * 30)
+
+
+# Solution from the site:
