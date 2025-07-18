5f5637ef652215ede23ee380f7a7c00d4687a4e5
IgorNemo <ig.dan2020@gmail.com>
2025-07-18 12:43:09 +0300

    part03-18 solution

--- part03-18_substrings_part_2/src/substrings_part_2.py
+++ part03-18_substrings_part_2/src/substrings_part_2.py
@@ -1,1 +1,15 @@
 # Write your solution here
+word = input("Please type in a string: ")
+n = len(word)
+while n > 0:
+    print(word[n - 1:len(word)])
+    n -= 1
+
+# Solution from the site:
+
+# string = input("Please type in a string: ")
+ 
+# start = len(string) - 1
+# while start >= 0:
+#     print(string[start:])
+#     start -= 1
