c7457ea80fa06652069a4fe3e2a66163679ee4d8
IgorNemo <ig.dan2020@gmail.com>
2025-07-18 14:38:54 +0300

    part03-19 solution

--- part03-19_does_it_contain_vowels/src/does_it_contain_vowels.py
+++ part03-19_does_it_contain_vowels/src/does_it_contain_vowels.py
@@ -1,1 +1,39 @@
 # Write your solution here
+# input_string = input("Please type in a string: ")
+# if "a" in input_string:
+#         print("a found")
+# else:
+#         if "e" in input_string:
+#             print("e found")
+#         else:
+#             if "o" in input_string:
+#                 print("o found")
+#             else:
+input_string = input("Please type in a string: ")
+if "a" in input_string:
+    print("a found")
+else:
+    print("a not found")
+if "e" in input_string:
+    print("e found")
+else:
+    print("e not found")
+if "o" in input_string:
+    print("o found")
+else:
+    print("o not found")
+
+
+# Solution from the site:
+
+# string = input("Please type in a string: ")
+# vowels = "aeo"
+# index = 0
+ 
+# while index < len(vowels):
+#     vowel = vowels[index]
+#     if vowel in string:
+#         print(vowel, "found")
+#     else:
+#         print(vowel, "not found")
+#     index += 1
