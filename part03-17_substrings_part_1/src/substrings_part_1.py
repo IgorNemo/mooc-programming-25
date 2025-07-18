90dfc1cf27bd09e15dc16186a91d501adb156d0c
IgorNemo <ig.dan2020@gmail.com>
2025-07-18 09:44:35 +0300

    part03-17 solution

--- part03-17_substrings_part_1/src/substrings_part_1.py
+++ part03-17_substrings_part_1/src/substrings_part_1.py
@@ -1,1 +1,16 @@
 # Write your solution here
+word = input("Please type in a string: ")
+n = 0
+while n < len(word):
+    print(word[0:(n + 1)])
+    n = n + 1
+
+# Solution from the site
+
+# string = input("Please type in a string: ")
+ 
+# length = 1
+# while length <= len(string):
+#     print(string[0:length])
+#     length += 1
+ 
