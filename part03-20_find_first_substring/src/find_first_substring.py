1081d915a88a6300644d9fc1b638355e2509a1ed
IgorNemo <ig.dan2020@gmail.com>
2025-07-24 14:19:13 +0300

    part03-20 solution

--- part03-20_find_first_substring/src/find_first_substring.py
+++ part03-20_find_first_substring/src/find_first_substring.py
@@ -1,1 +1,17 @@
 # Write your solution here
+word = input("Please type in a word: ")
+character = input("Please type in a character: ")
+index = word.find(character)
+length = len(word)
+if length >= index + 3:
+    print(word[index:index + 3])
+
+# Solution from the site:
+
+# word = input("Please type in a word: ")
+# character = input("Please type in a character: ")
+ 
+# index = word.find(character)
+# if index!=-1 and len(word)>=index+3:
+#     print(word[index:index+3])
+
