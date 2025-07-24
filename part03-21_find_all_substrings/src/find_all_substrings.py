a832b233c83e38fdee86fade7b40a07ab96a3e2d
IgorNemo <ig.dan2020@gmail.com>
2025-07-24 16:24:07 +0300

    part03-21 solution

--- part03-21_find_all_substrings/src/find_all_substrings.py
+++ part03-21_find_all_substrings/src/find_all_substrings.py
@@ -1,1 +1,26 @@
 # Write your solution here
+word = input("Please type in a word: ")
+character = input("Please type in a character: ")
+# index = word.find(character)
+# length = len(word)
+while len(word) >= 3:
+    index = word.find(character)
+    if index!=-1 and len(word)>=index+3:
+        print(word[index:index+3])
+        word = word[index + 1:len(word)]
+    else:
+        word = word[1:len(word)]
+
+# Solution from the site:
+
+# word = input("Please type in a word: ")
+# character = input("Please type in a character: ")
+ 
+# index = 0
+ 
+# while index + 3 <= len(word):
+#     if word[index] == character:
+#         print(word[index:index+3])
+#     index += 1
+
+
